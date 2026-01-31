654e158a891d666cef52f82fbaeb9933369a0b7f
alex4682 <smoliarenko.sasha@gmail.com>
2026-01-31 13:22:00 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Suspense } from "react";
 import { Main } from "./components/main/Main";
 import { Movie } from "./components/movies/Movie";
 import { Movies } from "./components/movies/Movies";
@@ -7,14 +8,18 @@ import { Reviews } from "./components/movies/Reviews";
 
 export const App = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="/movies" element={<Movies />} />
-                <Route path="/movies/:id" element={<Movie />} />
-                <Route path="/movies/:id/actors" element={<Actors />} />
-                <Route path="/movies/:id/reviews" element={<Reviews />} />
-            </Routes>
+        <BrowserRouter basename="/moviesearch">
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/movies" element={<Movies />} >
+                        <Route path="/movies/:id" element={<Movie />} >
+                            <Route path="/movies/:id/actors" element={<Actors />} />
+                            <Route path="/movies/:id/reviews" element={<Reviews />} />
+                        </Route>
+                    </Route>
+                </Routes>
+            </Suspense>
         </BrowserRouter>
     )
 }
